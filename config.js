cea2ef4050d4a1490c098440ab9d35a17dbbc4a6
FrealacOf <85633397+FrealacOf@users.noreply.github.com>
2021-08-15 14:35:27 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+exports.TOKEN = 'ODYzMTk2Mjk2OTM1MjQzNzg3.YOjYMg.OfVg-wkeD2qbQtU2t8V8eyt3--4'; //https://discord.com/developers/applications
+
+exports.DBL_API_KEY = ''; //https://top.gg
+
+exports.PREFIX = 'n!';
+
+exports.GOOGLE_API_KEY = 'AIzaSyDFitFhU8_vpxTctwS1wGMnRiuRjwGwjLw'; // https://console.cloud.google.com/apis/library/youtube.googleapis.com
+
+exports.GENIUS_API_KEY = 'suTHNveR0LDqaIWuZZnp_f6jQ0z7_y9uGzuIiO44HPMsvXkCJ2X1EelsuIr4Xsn7'; // https://genius.com/developers
+
+exports.yandex_API = 'pdct.1.1.20210719T103803Z.7cbfccbd21262b84.e516880dc5456d07eb230b4973afd0f6d6386166'; // https://yandex.com/dev/predictor/keys/get/
+
+exports.news_API = '8b8677c0aeef47898c2d68dd84d365a0'; // https://newsapi.org/
+
+exports.giphy_API = 'kYiVuLq8A9eg02Nk0Y3IjuvfP9kVN5Yc'; // https://developers.giphy.com/
+
+exports.AME_API = '5960f63d06c2ff677d809704d22c9bbecc7bb065727b373288223a73f203cf82cb50787b13a655116328ba6f7900ab3324ce9962a77c22e7c05759ead15d4852'; // https://api.amethyste.moe
+
+exports.blague_API = '5960f63d06c2ff677d809704d22c9bbecc7bb065727b373288223a73f203cf82cb50787b13a655116328ba6f7900ab3324ce9962a77c22e7c05759ead15d4852'; // https://blague.xyz/
